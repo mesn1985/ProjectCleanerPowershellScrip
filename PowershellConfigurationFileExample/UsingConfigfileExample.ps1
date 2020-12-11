a492acdb0dd4f0d5028cedf6736a121e0c7fc1a1
Martin Edwin Shcjødt Nielsen <lightmaze16@hotmail.com>
2020-12-11 19:06:53 +0100

    using config file example  cleand

--- PowershellConfigurationFileExample/UsingConfigfileExample.ps1
+++ PowershellConfigurationFileExample/UsingConfigfileExample.ps1
@@ -1,7 +1,9 @@
-$configuration = Import-PowerShellDataFile .\configurationFile.psd1
+
+$configuration = Import-PowerShellDataFile ($PSScriptRoot+"\configurationFile.psd1")
 
 $configuration.Database | Write-Host
 
 $configuration.Service1 | Write-Host
 
 $configuration.Service2 | Write-Host
+
