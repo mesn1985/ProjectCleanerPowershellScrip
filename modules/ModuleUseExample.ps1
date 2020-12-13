799e2679c7b4c4c571f1bc9c65a21d09df12721d
Martin Edwin Shcjødt Nielsen <lightmaze16@hotmail.com>
2020-12-13 15:39:14 +0100

    Module examples added

--- modules/ModuleUseExample.ps1
+++ modules/ModuleUseExample.ps1
@@ -1,3 +1,9 @@
+#Requires -Modules ModuleExample
 Import-Module .\ModuleExample
 
-ExampleFunction4
+write-host "All paths from where modules can be imported" -ForegroundColor Yellow 
+write-host ($env:PSModulePath -replace";","`n") -ForegroundColor Blue
+Write-host "Note that the path for the example module is not represented.`nincluded module by name convention is not shown in the enviromentvariable"
+Write-Host "To avoid using nameing convention on subfolders, modules should be moved to one of the displayed folders"
+
+ExampleFunction4 | Write-Host
